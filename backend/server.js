206f82c49295e3fbf908f3ccdf212d4d7d593dd0
Praba <prama1303@gmail.com>
2022-03-23 19:39:51 +0100

    MongoDB connected

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const colors = require('colors');
 const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
 const dotenv = require('dotenv').config();
 const app = express();
 
 app.use(express.json());
 
 const PORT = process.env.PORT || 6000;
-
+connectDB();
 //app.get('/api/goals', (req, res) => {
 //res.send('Get Goals');
 //res.json({ message: 'Get Goals' });
